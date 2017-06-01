12e31d2765234dadd65b62184d7e7ce9eb730049
Thibault de Lambilly <tdelambilly@eventual.biz>
2017-06-01 23:09:51 +0200

    un peu de francais

--- src/AdminBundle/Form/PollType.php
+++ src/AdminBundle/Form/PollType.php
@@ -3,6 +3,7 @@
 namespace AdminBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,7 +14,10 @@ class PollType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('person')->add('question')->add('answer');
+        $builder
+            ->add('person')
+            ->add('question')
+            ->add('answer');
     }
     
     /**
